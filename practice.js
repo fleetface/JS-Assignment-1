e320d434d136e4426b542042cd1b7c0377e91d27
M.Aliraza <techniqueszack@gmail.com>
2024-01-02 23:53:27 +0500

    It is JS Assignment 1

---
+++ practice.js
@@ -0,0 +1,21 @@
+//Multiply
+
+let num1 = 50;
+let num2 = 78;
+let sum = num1*num2
+console.log("Multiply: "+sum);
+
+//Divide
+
+let numb1 = 10
+let numb2 = 2
+let sum1 = numb1/numb2
+console.log("Divide: "+sum1);
+
+//sum a+b*c find the result
+
+let a = 12
+let b = 8
+let c = 5
+let sum2 = a+b*c
+console.log("Sum a+b*c: "+sum2)
